0d0b4919b237c8b0aadcb6249a861bd69781b7d3
chenweiguo <cwg_08@126.com>
2019-06-17 11:17:02 +0800

    高德地图测试 	new file:   app/.gitignore 	new file:   app/build.gradle 	new file:   app/libs/AMap_Location_V3.7.0_20171218.jar 	new file:   app/libs/AMap_Search_V6.9.0_20190530.jar 	new file:   app/libs/Android_Map3D_SDK_V6.9.0_20190530.jar 	new file:   app/proguard-rules.pro 	new file:   app/src/androidTest/java/com/example/gaode/ExampleInstrumentedTest.java 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/example/gaode/Main2Activity.java 	new file:   app/src/main/java/com/example/gaode/MainActivity.java 	new file:   app/src/main/jniLibs/arm64-v8a/libAMapSDK_MAP_v6_9_0.so 	new file:   app/src/main/jniLibs/armeabi-v7a/libAMapSDK_MAP_v6_9_0.so 	new file:   app/src/main/jniLibs/armeabi/libAMapSDK_MAP_v6_9_0.so 	new file:   app/src/main/jniLibs/x86/libAMapSDK_MAP_v6_9_0.so 	new file:   app/src/main/jniLibs/x86_64/libAMapSDK_MAP_v6_9_0.so 	new file:   app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   app/src/main/res/drawable/ic_launcher_background.xml 	new file:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/layout/activity_main2.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/styles.xml 	new file:   app/src/test/java/com/example/gaode/ExampleUnitTest.java 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle
     Untracked files:

---
+++ app/src/main/java/com/example/gaode/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.gaode;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.MyLocationStyle;
+
+public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends Activity implements LocationSource, AMapLocationListener,OnCheckedChangeListener {
+
+    MapView mMapView = null;
+
+    private AMap aMap;
+    private LocationSource.OnLocationChangedListener mListener = null;//定位监听器
+    //private LocationUtil locationUtil;
+    private static final int STROKE_COLOR = Color.argb(180, 3, 145, 255);
+    private static final int FILL_COLOR = Color.argb(10, 0, 0, 180);
+
+    private RadioGroup mGPSModeGroup;
+    private TextView mLocationErrText;
+    // 处理定位更新
+    //private LocationSource.OnLocationChangedListener mListener;
+    // 定位
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Main2Activity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+        init();
+    }
+    private void init() {
+        if(aMap == null){
+            aMap = mMapView.getMap();
+            //设置显示定位按钮 并且可以点击
+            UiSettings settings = aMap.getUiSettings();
+            //aMap.setLocationSource(this);//设置了定位的监听
+            // 是否显示定位按钮
+            settings.setMyLocationButtonEnabled(true);
+            aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
+
+        }
+
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocatio=nStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
+        //myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+        myLocationStyle.showMyLocation(true);
+        myLocationStyle.anchor(1, 1);
+        myLocationStyle.strokeColor(Color.BLUE);
+        myLocationStyle.strokeColor(Color.GREEN);
+        myLocationStyle.strokeWidth(1);
+        /*
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        //myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
+        /*myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) ;//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE);//连续定位、且将视角移动到地图中心点，地图依照设备方向旋转，定位点会跟随设备移动。（1秒1次定位）
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+        //以下三种模式从5.1.0版本开始提供
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，地图依照设备方向旋转，并且蓝点会跟随设备移动。
+    */
+        //设置SDK 自带定位消息监听
+       // aMap.setOnMyLocationChangeListener(this);
+    }
+
+
+    //定位激活回调
+    /*@Override
+    public void activate(LocationSource.OnLocationChangedListener onLocationChangedListener) {
+        //mListener = onLocationChangedListener;
+
+        //locationUtil.startLocate(getApplicationContext());
+    }*/
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Main2Activity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+}
